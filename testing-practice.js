6c989c79c65229642a8635a7b4c5407c9952be0b
Ryan McEntire <107779145+RyanMcEntire@users.noreply.github.com>
2023-05-05 21:29:20 -0600

    second first commit

---
+++ testing-practice.js
@@ -0,0 +1,41 @@
+export function capitalize(input) {
+  return input[0].toUpperCase() + input.slice(1);
+}
+
+export function reverseString(input) {
+  let reversed = '';
+  for (let i = input.length - 1; i >= 0; i--) {
+    reversed += input[i];
+  }
+  return reversed;
+}
+console.log(reverseString('Ryan'));
+
+export const calculator = {
+  add: (n1, n2) => n1 + n2,
+  subtract: (n1, n2) => n1 - n2,
+  divide: (n1, n2) => n1 / n2,
+  multiply: (n1, n2) => n1 * n2,
+};
+
+// vanilla letter abcdefghijklmnopqrstuvwxyz
+// offset of -10  qrstuvwxyzabcdefghijklmnop
+
+export function caesarCipher(string) {
+  const origin = 'abcdefghijklmnopqrstuvwxyz';
+  let offset = -10;
+  let cipherText = '';
+
+  for (let i = 0; i < string.length; i++) {
+    if (!/^[a-zA-Z]$/.test(string[i])) {
+      cipherText += string[i];
+    } else {
+      const isUpperCase = string[i] === string[i].toUpperCase();
+      const char = isUpperCase ? string[i].toLowerCase() : string[i];
+      const cipheredIndex = (origin.indexOf(char) + offset + origin.length) % origin.length;
+      const newChar = origin[cipheredIndex];
+      cipherText += isUpperCase ? newChar.toUpperCase() : newChar;
+    }
+  }
+  return cipherText;
+}
---
+++ testing-practice.test.js
@@ -0,0 +1,32 @@
+import {
+  capitalize,
+  reverseString,
+  calculator,
+  caesarCipher,
+} from './testing-practice';
+
+test('returns string with first letter capitalized', () => {
+  expect(capitalize('always')).toBe('Always');
+  expect(capitalize('forever')).toBe('Forever');
+  expect(capitalize('forEVER')).toBe('ForEVER');
+});
+
+test('returns a reversed string', () => {
+  expect(reverseString('always')).toBe('syawla');
+  expect(reverseString('forever')).toBe('reverof');
+  expect(reverseString('Space Jam')).toBe('maJ ecapS');
+});
+
+test('returns correct answer for the operation', () => {
+  expect(calculator.add(33, 3)).toBe(36);
+  expect(calculator.subtract(33, 3)).toBe(30);
+  expect(calculator.divide(33, 3)).toBe(11);
+  expect(calculator.multiply(33, 3)).toBe(99);
+});
+
+test('returns shifted text', () => {
+  expect(caesarCipher('good')).toBe('weet');
+  expect(caesarCipher('Good')).toBe('Weet');
+  expect(caesarCipher('Good. Well done!')).toBe('Weet. Mubb tedu!');
+});
+
